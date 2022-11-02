859f8785e2f5d667eddda4bd3f603014d7d9eb86
Hyunwoo Lee <hw5773@gmail.com>
2022-11-02 16:03:53 +0900

    update the host script

--- scripts/host.sh
+++ scripts/host.sh
@@ -1,11 +1,11 @@
 #/bin/sh
 
-sudo ip link add vowifi-gw-310210 type bridge
-sudo ip link set vowifi-gw-310210 up
-sudo ip address add fdad:dabb:ed::1 dev vowifi-gw-310210
-sudo route -A inet6 add fdad:dabb:ed::/64 dev vowifi-gw-310210
+sudo ip link add vowifi-310210 type bridge
+sudo ip link set vowifi-310210 up
+sudo ip address add fdad:dabb:ed::1 dev vowifi-310210
+sudo route -A inet6 add fdad:dabb:ed::/64 dev vowifi-310210
 
-sudo ip link add vowifi-gw-310260 type bridge
-sudo ip link set vowifi-gw-310260 up
-sudo ip address add fdad:dabb:ee::1 dev vowifi-gw-310260
-sudo route -A inet6 add fdad:dabb:ee::/64 dev vowifi-gw-310260
+sudo ip link add vowifi-310260 type bridge
+sudo ip link set vowifi-310260 up
+sudo ip address add fdad:dabb:ee::1 dev vowifi-310260
+sudo route -A inet6 add fdad:dabb:ee::/64 dev vowifi-310260
