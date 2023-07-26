f8ad75e3aa0eb36ab2d174e676bf595b40ebd9b6
Hyunwoo Lee <hw5773@gmail.com>
2023-07-26 01:03:08 +0000

    update the running script of P-CSCF for 310210

--- settings/ims/310210/pcscf/run.sh
+++ settings/ims/310210/pcscf/run.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
+
 sudo mkdir -p /var/run/kamailio_pcscf
-sudo kamailio -f kamailio.cfg -P /kamailio_pcscf.pid -DDeE
+
+if [ $# -eq 0 ]; then
+  sudo kamailio -f kamailio.cfg -P /kamailio_pcscf.pid -DDeE
+elif [ $# -eq 1 ]; then
+  if [ "$1" = "pixel" ]; then
+    sudo kamailio -f kamailio-pixel.cfg -P /kamailio_pcscf.pid -DDeE
+  else
+    sudo kamailio -f kamailio.cfg -P /kamailio_pcscf.pid -DDeE
+  fi
+else
+  echo "invalid command"
+fi
