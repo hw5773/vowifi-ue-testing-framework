c811a861526d2111768b74e0ac83fd970d37accb
seulgihan20230172 <132075972+seulgihan20230172@users.noreply.github.com>
2023-08-18 08:02:59 +0900

    Update ue_controller.py

--- learner/ue_controller/ue_controller.py
+++ learner/ue_controller/ue_controller.py
@@ -17,7 +17,7 @@ def handle_turn_off_wifi_interface(device):
     if device == "SM_G920T":
         cmd = ["adb", "shell", "su", "-c", "svc", "wifi", "disable"]
         subprocess.run(cmd)
-    elif device == "moto_e5_plus" or device == "HTC_U11" or device == "Blackview_A55" or device == "OnePlus Nord N20":
+    elif device == "moto_e5_plus":
         cmd = ["adb", "shell", "dumpsys", "wifi", "|", "grep", "\"Wi-Fi is\""]
         result = subprocess.run(cmd, stdout=subprocess.PIPE)
 
@@ -51,7 +51,7 @@ def handle_turn_on_wifi_interface(device):
     if device == "SM_G920T":
         cmd = ["adb", "shell", "su", "-c", "svc", "wifi", "enable"]
         subprocess.run(cmd)
-    elif device == "moto_e5_plus" or device == "HTC_U11" or device == "Blackview_A55" or device =="OnePlus Nord N20":
+    elif device == "moto_e5_plus":
         cmd = ["adb", "shell", "dumpsys", "wifi", "|", "grep", "\"Wi-Fi is\""]
         result = subprocess.run(cmd, stdout=subprocess.PIPE)
 
@@ -216,12 +216,30 @@ def check_device_model():
     elif "HTC_U11" in output:
         device = "HTC_U11"
         logging.info("Device model: HTC U11")
-    elif "Blackview_A55" in output:
-        device = "Blackview_A55"
-        logging.info("Device model: A55")
-    elif "OnePlus Nord N20" in output:
-        device = "OnePlus Nord N20"
-        logging.info("Device model: CPH2459")
+    elif "A55" in output:
+        device = "A55"
+        logging.info("Device model: Blackview_A55")
+    elif "CPH2459" in output:
+        device = "CPH2459"
+        logging.info("Device model: OnePlus Nord N20")
+    elif "A13_Pro" in output:
+        device = "A13_Pro"
+        logging.info("Device model: UMIDIGI A13 Pro")
+    elif "A2020N3" in output:
+        device = "A2020N3"
+        logging.info("Device model: ZTE Stage 5G")
+    elif "Pixel_6a" in output:
+        device = "Pixel_6a"
+        logging.info("Device model: Pixel 6a Android 12")
+    elif "SOV41" in output:
+        device = "SOV41"
+        logging.info("Device model: Xperia 5")
+    elif "2201117TG" in output:
+        device = "2201117TG"
+        logging.info("Device model: Redmi Note 2")
+    elif "LG_Q730" in output:
+        device = "LG_Q730"
+        logging.info("Device model: LG Stylo 6")
     else:
         device = "others"
         logging.info("Device model: Others")
