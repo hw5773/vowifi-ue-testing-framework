000344e7a05f040492d08c49068424587205c194
Hyunwoo Lee <hw5773@gmail.com>
2023-10-18 18:37:13 +0900

    update the primary testcases

--- scripts/generate_testcases.py
+++ scripts/generate_testcases.py
@@ -20,12 +20,15 @@ def abstract_testcases(pname):
     logging.info("Abstract> {} primary properties are ready".format(cnt))
     return ret
 
-def substitution(ptcs, ename):
+def substitution(ptcs, ename, fname):
     ret = {}
     ret["testcases"] = []
 
     errors = open(ename, "r").read()
-    emsgs = json.loads(errors)["errors"]
+    emsgs = json.loads(errors)["ike_errors"]
+
+    errors = open(fname, "r").read()
+    fmsgs = json.loads(errors)["sip_errors"]
     cnt = 0
 
     for ptc in ptcs:
@@ -36,14 +39,26 @@ def substitution(ptcs, ename):
         for testcase in testcases:
             tc = testcase["testcase"]
 
-            for emsg in emsgs:
-                t = {}
-                t["testcase"] = copy.deepcopy(tc[:-1])
-                t["testcase"].append(emsg)
-                tnum += 1
-                t["testcase"][0]["id"] = "{}-{}".format(ptc.get_property_number(), tnum)
-                ret["testcases"].append(t)
-                cnt += 1
+            lc = tc[-1]
+            if "ike" in lc["name"]:
+                for emsg in emsgs:
+                    t = {}
+                    t["testcase"] = copy.deepcopy(tc[:-1])
+                    t["testcase"].append(emsg)
+                    tnum += 1
+                    t["testcase"][0]["id"] = "{}-{}".format(ptc.get_property_number(), tnum)
+                    ret["testcases"].append(t)
+                    cnt += 1
+            elif "sip" in lc["name"]:
+                for emsg in fmsgs:
+                    t = {}
+                    t["testcase"] = copy.deepcopy(tc[:-1])
+                    t["testcase"].append(emsg)
+                    tnum += 1
+                    t["testcase"][0]["id"] = "{}-{}".format(ptc.get_property_number(), tnum)
+                    ret["testcases"].append(t)
+                    cnt += 1
+
 
     logging.info("Substitution> {} testcases are generated".format(cnt))
     return ret
@@ -178,11 +193,11 @@ def drop_payloads(ptcs):
     logging.info("Drop> {} testcases are generated".format(cnt))
     return ret
 
-def message_level_manipulation(ptcs, ename):
+def message_level_manipulation(ptcs, ename, fname):
     ret = {}
     ret["testcases"] = []
 
-    lst1 = substitution(ptcs, ename)
+    lst1 = substitution(ptcs, ename, fname)
     lst2 = replay(ptcs)
 
     for tc in lst1["testcases"]:
@@ -221,16 +236,17 @@ def write_testcases(ofname, mtcs, atcs):
     with open(ofname, "w") as of:
         of.write(json.dumps(testcases, indent=2))
 
-def generate_testcases(pname, ename, ofname):
+def generate_testcases(pname, ename, fname, ofname):
     ptcs = abstract_testcases(pname)
-    mtcs = message_level_manipulation(ptcs, ename)
+    mtcs = message_level_manipulation(ptcs, ename, fname)
     atcs = attribute_level_manipulation(ptcs)
     write_testcases(ofname, mtcs, atcs)
 
 def command_line_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--primary", metavar="<directory of primary properties>", help="Directory of Primary Properties", type=str, default="../testcases/primary")
-    parser.add_argument("-e", "--errors", metavar="<error messages file>", help="Error Message File", type=str, default="../testcases/errors/errors")
+    parser.add_argument("-e", "--ike-errors", metavar="<ike error messages file>", help="IKE Error Message File", type=str, default="../testcases/errors/ike_errors")
+    parser.add_argument("-f", "--sip-errors", metavar="<sip error messages file>", help="SIP Error Message File", type=str, default="../testcases/errors/sip_errors")
     parser.add_argument("-d", "--output-directory", metavar="<output directory>", help="Output directory", type=str, default="../testcases")
     parser.add_argument("-o", "--output", metavar="<output filename>", help="Output filename", type=str, default="testcases.json")
     parser.add_argument("-l", "--log", metavar="<log level (DEBUG/INFO/WARNING/ERROR/CRITICAL>", help="Log level (DEBUG/INFO/WARNING/ERROR/CRITICAL)", type=str, default="INFO")
@@ -246,8 +262,12 @@ def main():
         logging.error("No directory exists: {}".format(args.primary))
         sys.exit(1)
 
-    if not os.path.exists(args.errors):
-        logging.error("No error message exists: {}".format(args.errors))
+    if not os.path.exists(args.ike_errors):
+        logging.error("No IKE error message exists: {}".format(args.ike_errors))
+        sys.exit(1)
+
+    if not os.path.exists(args.sip_errors):
+        logging.error("No SIP error message exists: {}".format(args.sip_errors))
         sys.exit(1)
 
     if not os.path.exists(args.output_directory):
@@ -256,7 +276,7 @@ def main():
     ofname = "{}/{}".format(args.output_directory, args.output)
     logging.info(">>>> Output filename would be {}".format(ofname))
 
-    generate_testcases(args.primary, args.errors, ofname)
+    generate_testcases(args.primary, args.ike_errors, args.sip_errors, ofname)
 
 if __name__ == "__main__":
     main()
