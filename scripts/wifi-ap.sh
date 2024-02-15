0d57b631d7228d78e7b947658b590554dae329e9
Hyunwoo Lee <hw5773@gmail.com>
2024-02-15 09:05:45 +0900

    update the wifi ap script

--- scripts/wifi-ap.sh
+++ scripts/wifi-ap.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 
-if [[ "$#" -ne 2 ]]; then
+if [[ "$#" -ne 1 ]]; then
   echo "Illegal number of parameters"
-  echo "./wifi-ap.sh <up/down> <wifi interface name>"
+  echo "./wifi-ap.sh <wifi interface name>"
   exit 1
 fi
 
-if [[ "$1" == "up" ]]; then
-  echo "The AP mode is up"
-  echo "sudo ip address flush $2"
-  sudo ip address flush $2
-  echo "sudo wifi-ap.config set disabled=false"
-  sudo wifi-ap.config set disabled=false
-  echo "sudo wifi-ap.status restart-ap"
-  sudo wifi-ap.status restart-ap
-elif [[ "$1" == "down" ]]; then
-  echo "The AP mode is down"
-  echo "sudo ip address flush $2"
-  sudo ip address flush $2
-  echo "sudo wifi-ap.config set disabled=true"
-  sudo wifi-ap.config set disabled=true
-  echo "sudo dhclient -i $2"
-  sudo dhclient -i $2
-else
-  echo "Invalid argument (The first one should be either of up or down)"
-fi
+echo "The AP mode is up"
+echo "sudo ip address flush $1"
+sudo ip address flush $1
+echo "sudo ifconfig $1 172.24.1.1 netmask 255.255.255.0"
+sudo ifconfig $1 172.24.1.1 netmask 255.255.255.0
+echo "sudo dnsmasq -C /etc/dnsmasq.conf"
+sudo dnsmasq -C /etc/dnsmasq.conf
+echo "sudo hostapd -B /etc/hostapd/hostapd.conf"
+sudo hostapd -B /etc/hostapd/hostapd.conf
