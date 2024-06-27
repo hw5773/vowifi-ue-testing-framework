b9c57b52c9e718cfec34d2f61189cccdbebd39ca
ha0711 <hassan.asghar@kakao.com>
2024-06-27 18:26:38 +0900

    ue_controller.py updated

--- learner/ue_controller/ue_controller.py
+++ learner/ue_controller/ue_controller.py
@@ -27,7 +27,7 @@ def handle_turn_off_wifi_interface(device):
 
             cmd = ["adb", "shell", "am", "start", "-a", "android.intent.action.MAIN", "-n", "com.android.settings/.wifi.WifiSettings"]
             output = subprocess.run(cmd, stdout=subprocess.PIPE)
-            time.sleep(1)
+            time.sleep(3)
 
             cmd = ["adb", "shell", "input", "keyevent", "23"]
             subprocess.run(cmd)
@@ -206,16 +206,16 @@ def handle_init_config(device):
         result = subprocess.run(cmd, stdout=subprocess.PIPE)
         time.sleep(3)
         
-        logging.debug("Enabling swipe")
-        cmd = ["adb", "shell", "input", "swipe", "200", "500", "200", "0"]
-        result = subprocess.run(cmd, stdout=subprocess.PIPE)
-        time.sleep(3)
-        logging.debug("Enabled swipe")
+        #logging.debug("Enabling swipe")
+        #cmd = ["adb", "shell", "input", "swipe", "200", "500", "200", "0"]
+        #result = subprocess.run(cmd, stdout=subprocess.PIPE)
+        #time.sleep(3)
+        #logging.debug("Enabled swipe")
         
         logging.debug("Enabling wifi")
         cmd = ["adb", "shell", "svc", "wifi", "enable"]
         result = subprocess.run(cmd, stdout=subprocess.PIPE)
-        time.sleep(4)
+        time.sleep(3)
         logging.debug("Enabled wifi")
 
         logging.debug("Enable WiFi Calling")
@@ -225,7 +225,7 @@ def handle_init_config(device):
         logging.debug("Enabled Wifi Calling")
  
         logging.debug("Toggle the WiFi Calling button")
-        for _ in range(2):
+        for _ in range(1):
             cmd = ["adb", "shell", "input", "keyevent", "23"]
             result = subprocess.run(cmd, stdout=subprocess.PIPE)
             time.sleep(3)
