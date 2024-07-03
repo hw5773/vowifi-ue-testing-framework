15b3825554c3d967560f9f096a950403ea4bfa00
ha0711 <hassan.asghar@kakao.com>
2024-07-04 00:12:33 +0900

    ue_controller.py

--- learner/ue_controller/ue_controller.py
+++ learner/ue_controller/ue_controller.py
@@ -88,10 +88,10 @@ def ue_wakeup(device):
         if is_usb_dialog_present():
             logging.debug("Permission already given")
         else:
-        for _ in range(3):
-            cmd = ["adb", "shell", "input", "keyevent", "20"]
-            result = subprocess.run(cmd, stdout=subprocess.PIPE)
-            time.sleep(3)
+            for _ in range(3):
+                cmd = ["adb", "shell", "input", "keyevent", "20"]
+                result = subprocess.run(cmd, stdout=subprocess.PIPE)
+                time.sleep(3)
     cmd = ["adb", "shell", "input", "keyevent", "23"]
     result = subprocess.run(cmd, stdout=subprocess.PIPE)
     time.sleep(3)
