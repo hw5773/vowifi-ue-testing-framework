0727a76230108c68cb6b0706288644efb5b25b95
ha0711 <hassan.asghar@kakao.com>
2024-07-04 03:10:07 +0900

    ue_controller.py

--- learner/ue_controller/ue_controller.py
+++ learner/ue_controller/ue_controller.py
@@ -360,7 +360,7 @@ def handle_init_config(device):
         time.sleep(3)
         logging.debug("Enabled Wifi Calling")
         
-        if is_wifi_enabled():
+        if is_vowifi_enabled():
             logging.debug("Wifi is enabled")
         else:
             for _ in range(1):
