72a13bc3be7542c86c8ba57e88398b76cfb9384c
esslab002@esslab-002 <hassan.asghar@kakao.com>
2024-07-03 18:44:03 +0900

    ue_controller.py

--- learner/ue_controller/ue_controller.py
+++ learner/ue_controller/ue_controller.py
@@ -312,6 +312,15 @@ def handle_init_config(device):
         time.sleep(3)
         logging.debug("Enabled Wifi Calling")
 
+        logging.debug("Enabled Wifi Calling")
+
+        logging.debug("Toggle the WiFi Calling button")
+        for _ in range(2):
+            cmd = ["adb", "shell", "input", "keyevent", "23"]
+            result = subprocess.run(cmd, stdout=subprocess.PIPE)
+            time.sleep(3)
+        logging.debug("Finish toggling the WiFi Calling button")
+
 
 def handle_client_connection(client, server, device):
     logging.info("Client handler initiated")
