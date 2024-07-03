e299bc83ce57553ece488d9f74a4a716830380a8
ha0711 <hassan.asghar@kakao.com>
2024-07-03 15:43:02 +0900

    ue_controller.py

--- learner/ue_controller/ue_controller.py
+++ learner/ue_controller/ue_controller.py
@@ -180,6 +180,12 @@ def handle_init_config(device):
         time.sleep(3)
         logging.debug("Menu enabled successfully")
 
+        logging.debug("Enabling Wifi")
+        cmd = ["adb", "shell", "svc", "wifi", "enable"]
+        result = subprocess.run(cmd, stdout=subprocess.PIPE)
+        time.sleep(3)
+        logging.debug("Enabled Wifi")
+
         logging.debug("Enable WiFi Calling")
         cmd = ["adb", "shell", "am", "start", "-a", "android.intent.action.MAIN", "-n", "com.telephony.service/.wfc.WfcAliasActivity"]
         result = subprocess.run(cmd, stdout=subprocess.PIPE)
