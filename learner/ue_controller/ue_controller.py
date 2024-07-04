e5271ad635ac6d908b7fb5baecd555af70ed0abf
Hyunwoo Lee <hw5773@gmail.com>
2024-07-05 01:38:24 +0900

    ue_controller.py

--- learner/ue_controller/ue_controller.py
+++ learner/ue_controller/ue_controller.py
@@ -311,6 +311,37 @@ def handle_init_config(device):
             time.sleep(3)
         logging.debug("Finish toggling the WiFi Calling button")
 
+    elif device == "One_Plus_Nord_N20":
+        logging.debug("Menu to unlock the phone")
+        cmd = ["adb", "shell", "input", "keyevent", "82"]
+        result = subprocess.run(cmd, stdout=subprocess.PIPE)
+        time.sleep(3)
+
+        logging.debug("Enabling swipe")
+        cmd = ["adb", "shell", "input", "swipe", "200", "500", "200", "0"]
+        result = subprocess.run(cmd, stdout=subprocess.PIPE)
+        time.sleep(3)
+        logging.debug("Enabled Swipe")
+
+        logging.debug("Enabling wifi")
+        cmd = ["adb", "shell", "svc", "wifi", "enable"]
+        result = subprocess.run(cmd, stdout=subprocess.PIPE)
+        time.sleep(4)
+        logging.debug("Enabled wifi")
+
+        logging.debug ("Enable WiFi Calling")
+        cmd = ["adb", "shell", "am", "start", "-a", "android.intent.action.MAIN", "-n", "com.android.settings/.wifi.calling.WifiCallingSuggestionActivity"]
+        result = subprocess.run(cmd, stdout=subprocess.PIPE)
+        time.sleep(3)
+        logging.debug("Enabled Wifi Calling")
+
+        logging.debug("Toggle the WiFi Calling button")
+        for _ in range(2):
+            cmd = ["adb", "shell", "input", "keyevent", "23"]
+            result = subprocess.run(cmd, stdout=subprocess.PIPE)
+            time.sleep(3)
+        logging.debug("Finish toggling the WiFi Calling button")
+
 def handle_client_connection(client, server, device):
     logging.info("Client handler initiated")
     count_enable_vowifi = 0
