98af62e4ed4738b3eecd1de4c9c24e2450122235
Hyunwoo Lee <hw5773@gmail.com>
2024-07-08 00:17:54 +0900

    update the framework

--- learner/ue_controller/ue_controller.py
+++ learner/ue_controller/ue_controller.py
@@ -173,7 +173,10 @@ def is_vowifi_enabled():
     return "VoWifi enabled" in result_check.stdout
 
 def handle_init_config(device):
-    if device == "ZTE_Stage_5G":
+    cmd = ["adb", "shell", "svc", "power", "stayon", "true"]
+    result = subprocess.run(cmd, stdout=subprocess.PIPE)
+
+    if device == "Z8850K":
         #for _ in range(3):
             #cmd = ["adb", "shell", "input", "keyevent", "20"]
             #result = subprocess.run(cmd, stdout=subprocess.PIPE)
@@ -182,17 +185,18 @@ def handle_init_config(device):
         #result = subprocess.run(cmd, stdout=subprocess.PIPE)
         #time.sleep(3)
         
+        logging.debug("Closing the USB configuration menu")
+        cmd = ["adb", "shell", "input", "keyevent", "20"]
+        result = subprocess.run(cmd, stdout=subprocess.PIPE)
+        time.sleep(1)
+        cmd = ["adb", "shell", "input", "keyevent", "23"]
+        result = subprocess.run(cmd, stdout=subprocess.PIPE)
+
         logging.debug("Enabling Menu")
         cmd = ["adb", "shell", "input", "keyevent", "82"]
         result = subprocess.run(cmd, stdout=subprocess.PIPE)
         time.sleep(3)
         logging.debug("Enabled Menu")
-
-        logging.debug("Enabling Wifi")
-        cmd = ["adb", "shell", "svc", "wifi", "enable"]
-        result = subprocess.run(cmd, stdout=subprocess.PIPE)
-        time.sleep(3)
-        logging.debug("Enabled Wifi")
 
         logging.debug("Enable WiFi Calling")
         cmd = ["adb", "shell", "am", "start", "-a", "android.intent.action.MAIN", "-n", "com.telephony.service/.wfc.WfcAliasActivity"]
@@ -318,7 +322,7 @@ def handle_init_config(device):
             time.sleep(3)
         logging.debug("Finish toggling the WiFi Calling button")
 
-    elif device == "TCL40XL":
+    elif device == "T608M":
         logging.debug("Menu to unlock the phone")
         cmd = ["adb", "shell", "input", "keyevent", "82"]
         result = subprocess.run(cmd, stdout=subprocess.PIPE)
@@ -330,25 +334,6 @@ def handle_init_config(device):
         time.sleep(3)
         logging.debug("Enabled Swipe")
  
-        logging.debug("Enabling wifi")
-        cmd = ["adb", "shell", "svc", "wifi", "enable"]
-        result = subprocess.run(cmd, stdout=subprocess.PIPE)
-        time.sleep(4)
-        logging.debug("Enabled wifi")
- 
-        logging.debug ("Enable WiFi Calling")
-        cmd = ["adb", "shell", "am", "start", "-a", "android.intent.action.MAIN", "-n", "com.android.settings/.wifi.calling.WifiCallingSuggestionActivity"]
-        result = subprocess.run(cmd, stdout=subprocess.PIPE)
-        time.sleep(4)
-        logging.debug("Enabled Wifi Calling")
- 
-        logging.debug("Toggle the WiFi Calling button")
-        for _ in range(2):
-            cmd = ["adb", "shell", "input", "keyevent", "23"]
-            result = subprocess.run(cmd, stdout=subprocess.PIPE)
-            time.sleep(4)
-        logging.debug("Finish toggling the WiFi Calling button")
-
     elif device == "One_Plus_Nord_N20":
         logging.debug("Menu to unlock the phone")
         cmd = ["adb", "shell", "input", "keyevent", "82"]
@@ -404,6 +389,10 @@ def handle_client_connection(client, server, device):
                     name = "LG_Stylo_6"
                 elif name == "Note_14":
                     name = "Ulefone_Note_14"
+                elif name == "Z8850K":
+                    name = "ZTE_Stage_5G"
+                elif name == "T608M":
+                    name = "TCL_40XL"
                 logging.info("UE model name to be sent: {}".format(name))
                 client.send("{}\n".format(name).encode())
             elif opcode == "ue_reboot":
@@ -468,7 +457,7 @@ def check_device_model():
         device = "Blackview_A55"
         logging.info("Device model: A55")
     elif "Z8850K" in output:
-        device = "ZTE_Stage_5G"
+        device = "Z8850K"
         logging.info("Device model: ZTE STAGE 5G")
     elif "A55" in output:
         device = "A55"
@@ -492,7 +481,7 @@ def check_device_model():
         device = "LG_Q730"
         logging.info("Device model: LG Stylo 6")
     elif "T608M" in output:
-        device = "TCL40XL"
+        device = "T608M"
         logging.info("Device model: TCL 40XL")
     elif "I14_Pro_max" in output:
         device = "I14_Pro_Nax"
